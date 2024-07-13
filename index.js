d9715f55573c379adf28175d252532a00df6b794
Anas Mamlouk <anas.mamlouk@yahoo.fr>
2024-07-14 00:39:54 +0200

    Challenge 2

--- index.js
+++ index.js
@@ -12,9 +12,32 @@ const CashInRegister = 100;
 const OrderQueue = [];
 
 /*
-  challenge 1 : Add a utility function to add a new pizza to the menu
+  challenge 1 : 
+  Add a utility function to add a new pizza to the menu
 */
 
 function AddNewPizza(PizzaObj) {
   Menu.push(PizzaObj); 
 }
+
+/*
+  challenge 2 : 
+  Write another utility function, placeOrder, that takes a pizza name parameter and:
+  1. finds that pizza object in the menu,
+  2. adds the income to the cashInRegister,
+  3. pushes a new "order object” to the orderQueue
+  (e.g. { pizza: selectedPizzaObjectFromStepl, status: "ordered" }) 
+  4. returns the new order object (just in case we need it later)
+*/
+
+function PlaceOrder(PizzaName) {
+  const SelectedPizza = Menu.find(PizzaObj => PizzaObj === PizzaName)
+  CashInRegister += SelectedPizza.price
+  const NewOrder = {
+    Pizza: SelectedPizza,
+    status: "Ordered"
+  }
+  OrderQueue.push(NewOrder)
+  return NewOrder
+}
+
